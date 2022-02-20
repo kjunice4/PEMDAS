a5a449f01fa9ed6d91e066ad2697ba9a728429c7
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-02-20 15:44:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,27 @@ Builder.load_string("""
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
-                
+        
+        Button:
+            font_size: 50
+            background_color: 0, 0 , 0 , 1
+            size_hint_y: None
+            height: 200
+            text: "Tap to continue"
+            on_release:
+                app.root.current = "Menu"
+                root.manager.transition.direction = "left" 
+        
         Button:
             font_size: 40
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
-            height: 100
+            height: 200
             text: "KSquared-math,LLC © : PEMDAS Calculator"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
                 
-        Button:
-            font_size: 50
-            background_color: 0, 0 , 0 , 1
-            size_hint_y: None
-            height: 100
-            text: "Tap to continue"
-            on_release:
-                app.root.current = "Menu"
-                root.manager.transition.direction = "left" 
-
-
 """)
 
 # Menu
