4c4840320a1fa1178c316fa7c1b2cbdd8ff9309e
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-03-10 11:52:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -364,7 +364,10 @@ class PEMDAS(Screen):
                     a = replaced
                     print("a =",a)
                     i = i + 1 
-                    
+            
+            elif a.count("(") == 0 and a.count(")") == 0:
+                print("No Parenthesis, continue to exponents step")
+                
             else:
                 print("Parentheses Unbalanced!")
             
@@ -699,6 +702,8 @@ class PEMDAS(Screen):
                     if found_sub == -1:
                         break
                     print("found_sub",found_sub)
+                    if found_sub == 0:
+                        found_sub = a.rfind("-")
                     sub_left = a[:found_sub]
                     print("sub_left",sub_left)
                     if sub_left == "":
